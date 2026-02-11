fdd8cb49d74e790cfcf81e244558748f8b7587ba
VaidehiRupapara <168514690+VaidehiRupapara@users.noreply.github.com>
2026-02-11 14:35:22 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 23343
+let accountEmail = "vaidehisrupapara@gmail.com"
+var accountPassword = "2356"
+accountCity = "surat"
+let accountState;
+
+// accountId = 21 // not allowed
+
+accountEmail = "vr@gmail.com"
+accountPassword = "212121"
+accountCity = "zurat"
+
+console.log(accountId);
+
+console.table( [accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert (3 + 3); 
+console. log(3 
+    + 3)
+
+console. log("vaidehi")
+
+let name = "vaidehi"
+let age = 21
+let isLoggedIn = false;
+
+// number => 2 to power 53
+// bigint
+//string => ""
+// boolean => true/false
+// null => standalone value
+// undefiend =>
+// symbol => unique
+
+
+console. log(typeof age);
+
+
